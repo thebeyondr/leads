e9f2d04d5df9e6d9a8e83de34eed35383249c798
zextis <jeopatto@gmail.com>
2019-02-20 20:28:57 -0500

    Install nodemon

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 var express = require('express')
 var router = express.Router()
-const indexController = require('../controllers/index')
+const indexController = require('../controllers/landing')
 
 /* GET home page. */
-router.get('/', indexController.getIndex)
+router.get('/', indexController.getLanding)
 
 module.exports = router
